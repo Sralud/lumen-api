b72c2f366737bd31aaf494bdff73c723aac6285a
Sralud <llorente.gerald2@gmail.com>
2025-03-08 17:23:10 +0800

    updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,11 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\User;
+use App\Traits\ApiResponser;
+use DB;
+
 Class UserController extends Controller {
     private $request;
     public function __construct(Request $request){
         $this->request = $request;
  }
+
+    protected function successResponse($data, $code = Response::HTTP_OK){
+        return response()->json(['data' => $data], $code);
+    }
+
+    protected function errorResponse($message, $code){
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+    
     public function getUsers(){
         $users = User::all();
         return response()->json($users, 200);
