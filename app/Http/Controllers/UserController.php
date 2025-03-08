14e611a4f5e9ba4af0979bd5542ba0e0a9d3fbaf
Sralud <llorente.gerald2@gmail.com>
2025-03-08 16:55:06 +0800

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Models\User;
 Class UserController extends Controller {
     private $request;
