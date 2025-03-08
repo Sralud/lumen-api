8dbe925e56fcd55f559008569629179b88dac9b4
Sralud <llorente.gerald2@gmail.com>
2025-03-08 16:57:22 +0800

    updated

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ $router->get('/', function () use ($router) {
 });
 
 $router->group(['prefix' => 'api'], function () use ($router) {
-    $router->get('/users',['uses' => 'UserController@getUsers']);
-    $router->post('/users', ['uses' => 'UserController@createUser']);
+    $router->get('/users', 'UserController@index'); // get all users records
+    $router->post('/users', 'UserController@add'); // create new user record
+    $router->get('/users/{id}', 'UserController@show'); // get user by id
+    $router->put('/users/{id}', 'UserController@update'); // update user record
+    $router->patch('/users/{id}', 'UserController@update'); // update user record
+    $router->delete('/users/{id}', 'UserController@delete'); // delete record
 });
