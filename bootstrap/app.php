0bb89c89e59fd5cabd10cbdb20311a0cb4dbc1a2
Sralud <llorente.gerald2@gmail.com>
2025-04-14 16:08:10 +0800

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -91,7 +91,9 @@ $app->routeMiddleware([
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Laravel\Passport\PassportServiceProvider::class);
+$app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
